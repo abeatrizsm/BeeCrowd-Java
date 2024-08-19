f2e72b3b0e9ae7c61a5157e4835817d78685aba1
Ana Beatriz <anabeatriz.smendes@gmail.com>
2024-08-19 16:23:33 -0300

    1013

--- 1013.java
+++ 1013.java
@@ -1,3 +1,20 @@
-public class 1013 {
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner prompt = new Scanner(System.in);
+        int a = prompt.nextInt();
+        int b = prompt.nextInt();
+        int c = prompt.nextInt();
+
+        int maiorab = (a+b+Math.abs(a-b))/2;
+        int maior = (c + maiorab + Math.abs(c-maiorab))/2;
+
+        System.out.println(maior + " eh o maior");
+        prompt.close();
     
+    }
 }
+
