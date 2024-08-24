87c2dfff90f120e5f864c13dac3a4aeac1af6aa7
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-08-24 12:11:27 -0300

    1014

--- 1014.java
+++ 1014.java
@@ -1,3 +1,17 @@
-public class 1014 {
-    
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner prompt = new Scanner(System.in);
+        int x = prompt.nextInt();
+        Float y = prompt.nextFloat();
+        
+        Float consumo = x/y;
+
+        System.out.printf("%.3f km/l\n", consumo);
+        prompt.close();
+    }
 }
+
